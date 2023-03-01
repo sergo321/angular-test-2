f6ce02fbe5f917b271b121b70524efa8e9766c61
georgeneligan <108015805+georgeneligan@users.noreply.github.com>
2023-03-01 22:43:49 +0400

    Add files via upload

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,42 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.css"],
 })
 export class TestComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   imgCollection: Array<object> = [
     {
-      image: '../../assets/sliderimg/1.jpg',
-      thumbImage:'../../assets/sliderimg/1.jpg',
-           alt: 'Image 1',
-                 title: 'Image 1',
-    }, {
-      image: '../../assets/sliderimg/2.jpg',
-      thumbImage: '../../assets/sliderimg/2.jpg',
-         title: 'Image 2',
-      alt: 'Image 2'
-    }, {
-      image: '../../assets/sliderimg/3.jpg',
-      thumbImage: '../../assets/sliderimg/3.jpg', 
-         title: 'Image 3',
-      alt: 'Image 3'
-    }, {
-      image: '../../assets/sliderimg/4.jpg',
-      thumbImage: '../../assets/sliderimg/4.jpg', 
-        title: 'Image 4',
-      alt: 'Image 4'
-    }, {
-      image: '../../assets/sliderimg/5.jpg',
-      thumbImage:'../../assets/sliderimg/5.jpg',
-      title: 'Image 5',
-      alt: 'Image 5'
+      image: "../../assets/sliderimg/11.jpg",
+      thumbImage: "../../assets/sliderimg/11.jpg",
+      alt: "Image 1",
+      title: "Image 1",
     },
-];
+    {
+      image: "../../assets/sliderimg/22.jpg",
+      thumbImage: "../../assets/sliderimg/22.jpg",
+      title: "Image 2",
+      alt: "Image 2",
+    },
+    {
+      image: "../../assets/sliderimg/33.jpg",
+      thumbImage: "../../assets/sliderimg/33.jpg",
+      title: "Image 3",
+      alt: "Image 3",
+    },
+    {
+      image: "../../assets/sliderimg/44.jpg",
+      thumbImage: "../../assets/sliderimg/44.jpg",
+      title: "Image 4",
+      alt: "Image 4",
+    },
+    {
+      image: "../../assets/sliderimg/55.jpg",
+      thumbImage: "../../assets/sliderimg/55.jpg",
+      title: "Image 5",
+      alt: "Image 5",
+    },
+  ];
 }
